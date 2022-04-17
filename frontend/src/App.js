a8be2d481c4336a790fcb19987cd8ab66b9732e3
hamdanmajid125 <53685142+hamdanmajid125@users.noreply.github.com>
2022-04-17 07:21:32 +0500

    f

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css'
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/Login'
+import Home from './pages/Home';
+import WebForm from './pages/WebForm';
+import UserStoryInput from './pages/UserStoryInput';
+import WebFormDetails from './pages/WebFormDetails';
+
+
+const App = () => {
+
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/userstory" element={<UserStoryInput />} />
+        <Route exact path="/webform" element={<WebForm />} />
+        <Route exact path="/webformdetails" element={<WebFormDetails />} />
+
+
+
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
